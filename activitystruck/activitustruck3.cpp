5948bda3434bf9e3f5fffd335192f5f22a257bc3
taznimt <taznimanwar06@gmail.com>
2024-04-02 18:09:02 +0700

    add alamat desa

--- activitystruck/activitustruck3.cpp
+++ activitystruck/activitustruck3.cpp
@@ -16,5 +16,12 @@ int main() {
 	Mahasiswa mhs;
 	cout << "Nomor Mahasiswa = ";
 	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa = ";
+	getline(cin, mhs.nama);
+	cout << "Alamat Mahasiswa = " << endl;
+	cout << "\t nama Desa = ";
+	cin >> mhs.alamat.desa;
+	cout << "\t nama Kota = ";
+	cin >> mhs.alamat.kota;
 
 }
