0530f6fc74a96e675bdd68c0eaff619785096f29
taznimt <taznimanwar06@gmail.com>
2024-04-02 17:34:47 +0700

    input data mahasiswa

--- activitystruck/activitystruck.cpp
+++ activitystruck/activitystruck.cpp
@@ -13,6 +13,14 @@ struct Mahasiswa {
 
 int main() {
     Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
 }
 
 
