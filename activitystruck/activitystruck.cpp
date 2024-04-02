4996ffc1af5f13424eb958749467a0da0341a997
taznimt <taznimanwar06@gmail.com>
2024-04-02 17:25:58 +0700

    using namespace

--- activitystruck/activitystruck.cpp
+++ activitystruck/activitystruck.cpp
@@ -2,9 +2,15 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+int main() {
+    
+
 }
 
+    
+
+
+ 
---
+++ activitystruck/activitystruck2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+struct Mahasiswa {
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa {
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+
+    cout << "Alamat Mahasiswa : ";
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : " << mhs.alamat;
+    cout << "\n Umur : " << mhs.umur;
+}
