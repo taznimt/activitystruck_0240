5550f5ea6b086af086100dd6d220fdeb7e268305
taznimt <taznimanwar06@gmail.com>
2024-04-02 18:14:14 +0700

    display

--- activitystruck/activitystruck4.cpp
+++ activitystruck/activitystruck4.cpp
@@ -13,3 +13,22 @@ struct Mahasiswa {
 	DetailAlamat alamat;
 	int umur;
 };
+int main() {
+	Mahasiswa mhs[3];
+	for (int i = 0; i < 3; i++) {
+		cout << "Data Mahasiswa Ke- " << (i + 1) << ":" << endl;
+		cout << "Nomor Mahasiswa = ";
+		getline(cin, mhs[i].nim);
+		cout << "Nama Mahasiswa = ";
+		getline(cin, mhs[i].nama);
+		cout << "Alamat Mahasiswa = " << endl;
+		cout << "\t nama Desa = ";
+		cin >> mhs[i].alamat.desa;
+		cout << "\t nama Kota = ";
+		cin >> mhs[i].alamat.kota;
+		cout << "Umur Mahsiswa = ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << endl;
+	}
+}
