facd752dd3d56d52ece38836615c038c16de37a1
taznimt <taznimanwar06@gmail.com>
2024-04-02 17:36:41 +0700

    menampilkan data

--- activitystruck/activitystruck.cpp
+++ activitystruck/activitystruck.cpp
@@ -21,6 +21,12 @@ int main() {
     cin >> mhs.alamat;
     cout << "Umur Mahasiswa : ";
     cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : " << mhs.alamat;
+    cout << "\n Umur : " << mhs.umur;
 }
 
 
