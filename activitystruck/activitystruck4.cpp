2dbb5ed4225a8525838d83a62aed6f8b488db3c1
taznimt <taznimanwar06@gmail.com>
2024-04-02 18:12:04 +0700

    add strct alamat dan mahasiswa

---
+++ activitystruck/activitystruck4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
+};
