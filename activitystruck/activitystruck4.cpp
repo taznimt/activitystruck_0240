cab721a292cc1a05a4c08ea0085f4102a57d33b5
taznimt <taznimanwar06@gmail.com>
2024-04-02 18:15:52 +0700

    input data mahasiswa

--- activitystruck/activitystruck4.cpp
+++ activitystruck/activitystruck4.cpp
@@ -30,5 +30,20 @@ int main() {
 		cin >> mhs[i].umur;
 		cin.ignore(1, '\n');
 		cout << endl;
-	}
+
+		
+		
+		
+	for (int i = 0; i < 3; i++) {
+			cout << endl;
+			cout << "Data Mahasiswa Ke- " << (i + 1) << ":" << endl;
+			cout << "\n NIM = " << mhs[i].nim;
+			cout << "\n Nama = " << mhs[i].nama;
+			cout << "\n Alamat = ";
+			cout << "\n Desa = " << mhs[i].alamat.desa;
+			cout << "\n Kota = " << mhs[i].alamat.kota;
+			cout << "\n Umur = " << mhs[i].umur;
+			cout << endl;
+	} 
+
 }
